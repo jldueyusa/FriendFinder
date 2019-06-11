5f5ebeadec999f213633bdae47c4ad74fc0bf69b
Jennifer Duey <jlduey@gmail.com>
2019-06-11 03:45:39 -0400

    added routes

--- server.js
+++ server.js
@@ -4,3 +4,19 @@ var express = require("express");
 var app = express();
 
 var PORT = process.env.PORT || 8080;
+
+// CSS
+app.use(express.static(__dirname + "/app"));
+
+//app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use(bodyParser.text());
+app.use(express.json());
+
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
