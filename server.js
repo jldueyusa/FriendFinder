60339014f3d64ccff84fa9aae1f184df0c8e61c9
Jennifer Duey <jlduey@gmail.com>
2019-06-13 03:13:44 -0400

    working on server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Dependencies
 var express = require("express");
+//var bodyParser = require("body-parser");
+//var path = require("path");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-// CSS
+// For serving of static CSS
 app.use(express.static(__dirname + "/app"));
 
 //app.use(bodyParser.json());
