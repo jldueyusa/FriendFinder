2a028719af8ce02e5e5f6e76d42413488420ade3
Jennifer Duey <jlduey@gmail.com>
2019-06-13 17:50:36 -0400

    updated images not using google image and text

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 let friends = [
 	{
 		name: "Jon Snow",
-		photo: "https://images.app.goo.gl/18EogJLLm1iG6KKa8",
+		photo: "https://cimg.tvgcdn.net/i/r/2019/05/20/58d07e2f-41e6-4931-8144-4820228e555e/resize/1800x1200/596badfca2a28538cc3017d2092232ea/jon-snow-reg.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "Sansa Stark",
-		photo: "https://images.app.goo.gl/o2ETiF55Z8pSgPva6",
+		photo: "https://pmcdeadline2.files.wordpress.com/2019/05/sophie-turner-as-sansa-star-in-game-of-thrones.jpg?w=630",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Arya Stark",
-		photo: "https://images.app.goo.gl/S3phgZHVaXwExN51A",
+		photo: "https://gayexpress.co.nz/wp-content/uploads/2019/04/Maisie-online.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "Jaime Lannister",
-		photo: "https://images.app.goo.gl/dPunPXrMCkPzM9Mn9",
+		photo: "https://cdn.images.dailystar.co.uk/dynamic/117/photos/48000/620x/Jaime-Lannister-778849.jpg?r=5cdafa8573f5e",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
 		name: "Tyrion Lannister",
-		photo: "https://images.app.goo.gl/wAXcvgCw15T1CGdB8",
+		photo: "https://im.idiva.com/content/2019/Apr/idiva_tyrion-lannister_lead1.jpg?w=1100&h=513&q=75",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
