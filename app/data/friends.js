b12a537237fe24b7b306a6d4ae10921817c77180
Jennifer Duey <jlduey@gmail.com>
2019-06-13 02:37:53 -0400

    added friends

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+let friends = [
+	{
+		name: "Jon Snow",
+		photo: "https://images.app.goo.gl/18EogJLLm1iG6KKa8",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Sansa Stark",
+		photo: "https://images.app.goo.gl/o2ETiF55Z8pSgPva6",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Arya Stark",
+		photo: "https://images.app.goo.gl/S3phgZHVaXwExN51A",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Jaime Lannister",
+		photo: "https://images.app.goo.gl/dPunPXrMCkPzM9Mn9",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Tyrion Lannister",
+		photo: "https://images.app.goo.gl/wAXcvgCw15T1CGdB8",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
